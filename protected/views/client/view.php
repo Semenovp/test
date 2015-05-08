fc3cde4bd9ec91f2c9f23bd942b1186cf64c2238
semenovp <work.semenovp@gmail.com>
2015-05-08 17:46:43 +0300

    Вид после добавления клиентов

--- protected/views/client/view.php
+++ protected/views/client/view.php
@@ -1,33 +1,48 @@
 <?php
 /* @var $this ClientController */
 /* @var $model Client */
+//
+//$this->breadcrumbs=array(
+//	'Clients'=>array('index'),
+//	$model->name,
+//);
+//
+//$this->menu=array(
+//	array('label'=>'List Client', 'url'=>array('index')),
+//	array('label'=>'Create Client', 'url'=>array('create')),
+//	array('label'=>'Update Client', 'url'=>array('update', 'id'=>$model->id)),
+//	array('label'=>'Delete Client', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+//	array('label'=>'Manage Client', 'url'=>array('admin')),
+//);
+//?>
+<!---->
+<!--<h1>View Client #--><?php //echo $model->id; ?><!--</h1>-->
+<div class="alert alert-success" role="alert">Клиент успешно добавлен</div>
+<div class="panel panel-primary panel-material-light-blue-600">
 
-$this->breadcrumbs=array(
-	'Clients'=>array('index'),
-	$model->name,
-);
 
-$this->menu=array(
-	array('label'=>'List Client', 'url'=>array('index')),
-	array('label'=>'Create Client', 'url'=>array('create')),
-	array('label'=>'Update Client', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Client', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Client', 'url'=>array('admin')),
-);
-?>
-
-<h1>View Client #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
+	'htmlOptions' => array('class' => 'table table-striped table-hover'),
+	'itemCssClass' => array('active','active'),
 	'attributes'=>array(
-		'id',
 		'name',
-		'date',
+		'date' => array(
+			'label' => 'Дата',
+			'value' => date("d.m.Y H:i",$model->date)
+		),
 		'phone',
 		'email',
 		'company',
-		'status',
+		'status' => array(
+			'label' => 'Статус',
+			'value' => $model->StatusName->name
+		),
 		'hote',
 	),
 )); ?>
+</div>
+<?php
+	echo CHtml::link('Назад к клиентам', array('client/index'),array('class' => 'btn btn-primary btn-raised'));
+?>
