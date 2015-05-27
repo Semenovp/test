9585fdf12bd77a53a78e5db002337a99763f2b76
semenovp <work.semenovp@gmail.com>
2015-05-27 16:40:33 +0300

    альфа авторизации

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,7 +22,7 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-
+		'admin',
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'1234567',
@@ -49,7 +49,7 @@ return array(
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
-//			'showScriptName' => false,
+			'showScriptName' => false,
 		),
 
 
